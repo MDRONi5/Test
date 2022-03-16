e8c46ae16cc3e8b727a730fd94443bbcdd713dba
RONILX <54802274+MDRONi5@users.noreply.github.com>
2022-03-16 21:43:20 +0600

    Update udroid-impish-mate.sh

--- ubuntu-on-android/pd-plugins/impish/udroid-impish-mate.sh
+++ ubuntu-on-android/pd-plugins/impish/udroid-impish-mate.sh
@@ -6,7 +6,7 @@ DISTRO_COMMENT="A ubuntu 21.04 port with some tweaks xfce4"
 TARBALL_STRIP_OPT=0
 
 #TARBALL_URL['aarch64']="https://github.com/RandomCoderOrg/ubuntu-on-android/releases/download/v3/udroid-mate-arm64-betav2.tar.gz"
-TARBALL_URL['aarch64']="http://0.0.0.0:3000/RandomCoderOrg/udroid-mate-arm64-betav2.tar.gz"
+TARBALL_URL['aarch64']="http://0.0.0.0:3000/RandomCoderOrg/hippo-arm64-v02-xfce4.tar.gz"
 
 
 TARBALL_SHA256['aarch64']="3069a4c40c6d2982e943216f46ce52516a3c8d5ae29bdde633237ddd3748c88b"
